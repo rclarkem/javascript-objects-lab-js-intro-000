ee1b0e46661433de29d0ae3f62c5f1ce40b076ec
cheerful-animator-9488 <r.clarkemitchell@gmail.com>
2019-04-15 03:59:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ function updateObjectWithKeyAndValue(object, key, value){
   let newObj = Object.assign({},object,object[key])
   return newObj
 }
+
+updateObjectWithKeyAndValue(object, key, value){
+  let newObj = Object.assign(object,object[key])
+  return newObj
+}
